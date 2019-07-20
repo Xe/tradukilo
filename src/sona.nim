c742d4fdb7a24b7df02c94319cb01b86f1cc8d8b
Christine Dodrill <me@christine.website>
2019-07-20 13:23:57 -0400

    sona: parse into tokens

--- src/sona.nim
+++ src/sona.nim
@@ -1,4 +1,4 @@
-import json, options, os, streams, strformat, strutils
+import json, options, os, rdstdin, streams, strformat, strutils, tables
 
 var fName: string
 
@@ -19,15 +19,76 @@ type
     preposition = "prep",
     punctuation = "punct",
 
-  Word* = object of RootObj
+  Word* = ref object of RootObj
     name*: string
     grammar*: seq[WordKind]
     gloss*: string
     category*: Option[string]
 
+  PhraseKind* {.pure.} = enum
+    subject,
+    verb,
+    indirectObject,
+    directObject,
+
+  Phrase* = ref object
+    case kind*: PhraseKind
+    of PhraseKind.subject, PhraseKind.indirectObject, PhraseKind.directObject:
+      noun*: Word
+    of PhraseKind.verb:
+      preverb*: Word
+      verb*: Word
+
+    modifiers*: Option[seq[Word]]
+
+proc `$`*(w: Word): string =
+  if w.category.isSome:
+    result = fmt"{w.name}: grammar({w.grammar}), gloss({w.gloss}), category({w.category.get})"
+  else:
+    result = fmt"{w.name}: grammar({w.grammar}), gloss({w.gloss})"
+
+proc `$`*(p: Phrase): string =
+  var
+    base: string
+    modifiers: string
+
+  case p.kind
+  of PhraseKind.subject, PhraseKind.indirectObject, PhraseKind.directObject:
+    base = p.noun.name
+  of PhraseKind.verb:
+    base = p.verb.name
+
+  if p.modifiers.isSome:
+    var names = newSeq[string]()
+    for modif in p.modifiers.get:
+      names.add modif.name
+    modifiers = fmt"modifiers: {names}"
+
+  result = fmt"({p.kind}) {base} {modifiers}"
+
 var
   nodes = json.parseFile fName
   words = nodes.to(seq[Word])
+  dict = initTable[string, Word]()
 
 for word in words:
-  echo word
+  dict[word.name] = word
+
+var
+  rawSentence = readLineFromStdin "|toki: "
+  splitSentence = rawSentence.split " "
+  sentence = newSeq[Phrase]()
+  curr: Phrase
+  lastWord: Word
+
+for word in splitSentence:
+  if not dict.hasKey word:
+    quit fmt"unknown word: {word}"
+
+  let thisWord = dict[word]
+  echo fmt"found: {thisWord}"
+
+  for grm in thisWord.grammar:
+    echo grm
+
+  lastWord = thisWord
