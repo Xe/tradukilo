1961cc76af6df6908ba6a99e94b00113af87b242
Christine Dodrill <me@christine.website>
2019-07-21 16:59:16 -0400

    move toki pona code into a toki pona module

--- src/sona.nim
+++ src/sona.nim
@@ -1,92 +1,32 @@
 import json, options, os, rdstdin, streams, strformat, strutils, tables
-
-type
-  WordKind* {.pure.} = enum
-    ## Word types. These map to English terms, approximately.
-    noun = "n",
-    intransitiveVerb = "vi",
-    transitiveVerb = "vt",
-    modifier = "mod",
-    interjection = "interj",
-    conjunction = "conj",
-    separator = "sep",
-    preposition = "prep",
-    punctuation = "punct",
-
-  Word* = ref object of RootObj
-    ## An individual Toki Pona word.
-    name*: string
-    grammar*: seq[WordKind]
-    gloss*: string
-    category*: Option[string]
-
-  PhraseKind* {.pure.} = enum
-    ## The kind of phrase, used for parsing.
-    noun,
-    verb,
-
-  Phrase* = ref object
-    ## A phrase is a group of words.
-    case kind*: PhraseKind
-    of PhraseKind.noun:
-      noun*: Word
-    of PhraseKind.verb:
-      preverb*: Word
-      verb*: Word
-
-    modifiers*: Option[seq[Word]]
-
-proc isNoun*(w: Word): bool =
-  ## Returns true if the Word is a noun.
-  result = false
-
-  if WordKind.noun in w.grammar:
-    result = true
-
-proc isVerb*(w: Word): bool =
-  ## Returns true if the Word is a verb (transitive or intransitive).
-  result = false
-
-  if WordKind.intransitiveVerb in w.grammar or WordKind.transitiveVerb in w.grammar:
-    result = true
-
-proc isModifier*(w: Word): bool =
-  ## Returns true if the word is a modifier (~= adjective).
-  result = false
-
-  if WordKind.modifier in w.grammar:
-    result = true
-
-proc `$`*(w: Word): string =
-  ## Pretty-prints a Word.
-  if w.category.isSome:
-    result = fmt"{w.name}: grammar({w.grammar}), gloss({w.gloss}), category({w.category.get})"
-  else:
-    result = fmt"{w.name}: grammar({w.grammar}), gloss({w.gloss})"
-
-proc loadDictionary*(): TableRef[string, Word] =
-  ## Loads the toki pona dictionary into a Table from precompiled data.
-  const tpDictionary = staticRead "../data/tokipona.json"
-  var
-    s = newStringStream tpDictionary
-    nodes = s.parseJson
-    words = nodes.to seq[Word]
-  result = newTable[string, Word]()
-
-  for word in words:
-    result[word.name] = word
+import tradukilopkg/tokipona
 
 var
   dict = loadDictionary()
   rawSentence = readLineFromStdin "|toki: "
   splitSentence = rawSentence.split " "
-  lastWord: Word
+  lastWord: Option[Word]
+  first = true
 
 for word in splitSentence:
   if not dict.hasKey word:
     quit fmt"unknown word: {word}"
 
   let thisWord = dict[word]
-  echo fmt"found: {thisWord.name}, noun: {thisWord.isNoun}, verb: {thisWord.isVerb}, mod: {thisWord.isModifier}"
+  echo fmt"found: {thisWord}"
 
-  lastWord = thisWord
+  if lastWord.isSome:
+    let lw = lastWord.get
+
+    if not lw.isMiSina:
+      if lw.isNoun and not lw.isModifier:
+        raise GrammaticallyWrong(msg: fmt"invalid noun -> modifier transition: ({thisWord}) to ({lw})")
+
+      if lw.isNoun or lw.isModifier:
+        if thisWord.isVerb:
+          raise GrammaticallyWrong(msg: fmt"invalid noun -> verb transition: ({thisWord}) to ({lw})")
+
+    echo fmt"last:  {lw}"
+    first = false
+
+  lastWord = some thisWord
---
+++ src/tradukilopkg/tokipona.nim
@@ -0,0 +1,69 @@
+import json, options, streams, strformat, tables
+
+type
+  WordKind* {.pure.} = enum
+    ## Word types. These map to English terms, approximately.
+    noun = "n",
+    intransitiveVerb = "vi",
+    transitiveVerb = "vt",
+    modifier = "mod",
+    interjection = "interj",
+    conjunction = "conj",
+    separator = "sep",
+    preposition = "prep",
+    punctuation = "punct",
+
+  Word* = ref object of RootObj
+    ## An individual Toki Pona word.
+    name*: string
+    grammar*: seq[WordKind]
+    gloss*: string
+    category*: Option[string]
+
+proc isMiSina*(w: Word): bool =
+  ## Returns true if the word is "mi" or "sina", as these require special rules
+  case w.name
+  of "mi", "sina":
+    return true
+
+proc isNoun*(w: Word): bool =
+  ## Returns true if the Word is a noun.
+  result = WordKind.noun in w.grammar
+
+proc isVerb*(w: Word): bool =
+  ## Returns true if the Word is a verb (transitive or intransitive).
+  result = WordKind.intransitiveVerb in w.grammar or WordKind.transitiveVerb in w.grammar
+
+proc isTransitiveVerb*(w: Word): bool =
+  ## Returns true if the Word is a transitive verb.
+  result = WordKind.transitiveVerb in w.grammar
+
+proc isIntransitiveVerb*(w: Word): bool =
+  ## Returns true if the Word is a intransitive verb.
+  result = WordKind.intransitiveVerb in w.grammar
+
+proc isModifier*(w: Word): bool =
+  ## Returns true if the word is a modifier (~= adjective).
+  result = WordKind.modifier in w.grammar
+
+proc `$`*(w: Word): string =
+  ## Pretty-prints a Word.
+  if w.category.isSome:
+    result = fmt"{w.name}: grammar({w.grammar}), gloss({w.gloss}), category({w.category.get})"
+  else:
+    result = fmt"{w.name}: grammar({w.grammar}), gloss({w.gloss})"
+
+type GrammaticallyWrong* = ref object of ValueError
+
+proc loadDictionary*(): TableRef[string, Word] =
+  ## Loads the toki pona dictionary into a Table from precompiled data.
+  const tpDictionary = staticRead "../../data/tokipona.json"
+  var
+    s = newStringStream tpDictionary
+    nodes = parseJson s
+    words = nodes.to seq[Word]
+  result = newTable[string, Word]()
+
+  for word in words:
+    result[word.name] = word
+
